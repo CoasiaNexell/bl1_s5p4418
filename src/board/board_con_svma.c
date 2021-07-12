af6562f68d4d0a59eb4200e1d4dc7f3648b45fc0
eric.park <pjsin865@coasianexell.com>
2021-07-12 10:26:22 +0900

    bl1: board_con_svma.c: add the pmic for axp228
    This patch has changed so that nxe2000 and axp228 can be selected.
    
    Signed-off-by: eric.park <pjsin865@coasianexell.com>
    Change-Id: Iae075c847d02853e221dea0e820e0b39bad1b6aa

--- src/board/board_con_svma.c
+++ src/board/board_con_svma.c
@@ -20,48 +20,111 @@
 #include <i2c_gpio.h>
 #include <nxe2000.h>
 #include <mp8845.h>
-
-#define AUTO_VOLTAGE_CONTROL 		1
-#define ARM_VOLTAGE_CONTROL_SKIP	0
-
-#define NXE2000_I2C_GPIO_GRP		3 // GROUP : GPIOD
-#define NXE2000_I2C_SCL			4 // SCL   : GPIOD04
-#define NXE2000_I2C_SDA			5 // SDA   : GPIOD05
-#define NXE2000_I2C_SCL_ALT		0 // SCL   : ALT0
-#define NXE2000_I2C_SDA_ALT		0 // SDA   : ALT0
+#include <axp228.h>
 
 /**********************************************
- * apply Q100 condition *
- * ARM		: 1.125V
- * CORE		: 1.0V
- * DDR		: 1.5V
- * DDR_IO	: 1.5V
+ * PMIC I2C Info
 ************************************************/
-#define NXE2000_DEF_DDC1_Q100_VOL	1125000	/* VAL(uV) = 0: 0.60 ~ 3.5V, Step 12.5 mV, default(OTP) = 1.3V */
-#define NXE2000_DEF_DDC2_Q100_VOL	1000000	/* VAL(uV) = 0: 0.60 ~ 3.5V, Step 12.5 mV, default(OTP) = 1.1V */
-#define NXE2000_DEF_DDC3_Q100_VOL	3300000	/* VAL(uV) = 0: 0.60 ~ 3.5V, Step 12.5 mV, default(OTP) = 3.3V */
-#define NXE2000_DEF_DDC4_Q100_VOL	1500000	/* VAL(uV) = 0: 0.60 ~ 3.5V, Step 12.5 mV, default(OTP) = 1.6V */
-#define NXE2000_DEF_DDC5_Q100_VOL	1500000	/* VAL(uV) = 0: 0.60 ~ 3.5V, Step 12.5 mV, default(OTP) = 1.6V */
+#define PMIC_I2C_GPIO_GRP 3 // GPIOD
+#define PMIC_I2C_SCL 4 // SCL : GPIOD04
+#define PMIC_I2C_SDA 5 // SDA : GPIOD05
+#define PMIC_I2C_SCL_ALT 0 // SCL : ALT0
+#define PMIC_I2C_SDA_ALT 0 // SDA: ALT0
+
+/**********************************************
+ * Q100 condition
+ * ARM      : 1.125V
+ * CORE     : 1.0V
+ * SYS      : 3.3V
+ * DDR_IO   : 1.5V
+ * DDR      : 1.5V
+
+ * Consumer condition
+ * ARM      : 1.25V
+ * CORE     : 1.1V
+ * SYS      : 3.3V
+ * DDR_IO   : 1.5V
+ * DDR      : 1.5V
+************************************************/
+#define PMIC_DEF_ARM_VOL 1125000
+#define PMIC_DEF_CORE_VOL 1000000
+#define PMIC_DEF_3P3_VOL 3300000
+#define PMIC_DEF_DDRIO_VOL 1500000
+#define PMIC_DEF_DDR_VOL 1500000
 
 void pmic_board_init(void)
 {
 	char data[4];
 
-	I2C_INIT(NXE2000_I2C_GPIO_GRP, NXE2000_I2C_SCL, NXE2000_I2C_SDA,
-			NXE2000_I2C_SCL_ALT, NXE2000_I2C_SDA_ALT);
+	I2C_INIT(PMIC_I2C_GPIO_GRP, PMIC_I2C_SCL, PMIC_I2C_SDA,
+		PMIC_I2C_SCL_ALT, PMIC_I2C_SDA_ALT);
 
-	data[0] = nxe2000_get_dcdc_step(NXE2000_DEF_DDC1_Q100_VOL);
+#ifdef AXP228_PMIC_ENABLE
+	/* REG 80H : DC-DC Work mode
+	 *       bit 0 : DCDC1
+	 *           1 : DCDC2
+	 *           2 : DCDC3
+	 *           3 : DCDC4
+	 *           4 : DCDC5
+	 *   value 0 : PFM/PWM auto switch
+	 *           1 : fixed PWM */
+	axp228_read(AXP228_REG_DCDC_WORK_MODE, data, 1);
+	data[0] = (data[0] & 0x1F) | DCDC_SYS | DCDC_DDR;
+	axp228_write(AXP228_REG_DCDC_WORK_MODE, data, 1);
+
+	/* Set voltage of DCDC2(VCC_ARM) */
+	data[0] = axp228_get_dcdc_step(
+		PMIC_DEF_ARM_VOL, AXP228_DEF_DDC234_VOL_STEP,
+		AXP228_DEF_DDC234_VOL_MIN, AXP228_DEF_DDC24_VOL_MAX);
+	axp228_write(AXP228_REG_DC2VOL, data, 1);
+
+	/* Set voltage of DCDC3(VCC_CORE) */
+	data[0] = axp228_get_dcdc_step(
+		PMIC_DEF_CORE_VOL, AXP228_DEF_DDC234_VOL_STEP,
+		AXP228_DEF_DDC234_VOL_MIN, AXP228_DEF_DDC3_VOL_MAX);
+	axp228_write(AXP228_REG_DC3VOL, data, 1);
+
+	/*
+	 * VCC3P3_CORE(OTP:3.3V), VCC1P5_DDRIO(OTP:1.5V), VCC1P5_DDR(OTP:1.5V)
+	 * It is not set as it is the same as the OTP setting.
+	 * If it is different from OTP, please set it.
+	 */
+#if 0
+	/* Set voltage of DCDC1(VCC3P3_CORE) */
+	data[0] = axp228_get_dcdc_step(
+		PMIC_DEF_3P3_VOL, AXP228_DEF_DDC1_VOL_STEP,
+		AXP228_DEF_DDC1_VOL_MIN, AXP228_DEF_DDC1_VOL_MAX);
+	axp228_write(AXP228_REG_DC1VOL, data, 1);
+
+	/* Set voltage of DCDC4(VCC1P5_DDRIO) */
+	data[0] = axp228_get_dcdc_step(
+		PMIC_DEF_DDRIO_VOL, AXP228_DEF_DDC234_VOL_STEP,
+		AXP228_DEF_DDC234_VOL_MIN, AXP228_DEF_DDC24_VOL_MAX);
+	axp228_write(AXP228_REG_DC4VOL, data, 1);
+
+	/* Set voltage of DCDC5(VCC1P5_DDR) */
+	data[0] = axp228_get_dcdc_step(
+		PMIC_DEF_DDR_VOL, AXP228_DEF_DDC5_VOL_STEP,
+		AXP228_DEF_DDC5_VOL_MIN, AXP228_DEF_DDC5_VOL_MAX);
+	axp228_write(AXP228_REG_DC5VOL, data, 1);
+
+#endif
+
+#else /* NXE2000_PMIC_ENABLE */
+	data[0] = nxe2000_get_dcdc_step(PMIC_DEF_ARM_VOL);
 	nxe2000_write(NXE2000_REG_DC1VOL, data, 1);
 
-	data[0] = nxe2000_get_dcdc_step(NXE2000_DEF_DDC2_Q100_VOL);
+	data[0] = nxe2000_get_dcdc_step(PMIC_DEF_CORE_VOL);
 	nxe2000_write(NXE2000_REG_DC2VOL, data, 1);
 
-	data[0] = nxe2000_get_dcdc_step(NXE2000_DEF_DDC4_Q100_VOL);
+	data[0] = nxe2000_get_dcdc_step(PMIC_DEF_DDRIO_VOL);
 	nxe2000_write(NXE2000_REG_DC4VOL, data, 1);
 
-	data[0] = nxe2000_get_dcdc_step(NXE2000_DEF_DDC5_Q100_VOL);
+	data[0] = nxe2000_get_dcdc_step(PMIC_DEF_DDR_VOL);
 	nxe2000_write(NXE2000_REG_DC5VOL, data, 1);
+#endif
 
 	I2C_DEINIT();
 }
-#endif
+
+#endif /* PMIC_ON */
