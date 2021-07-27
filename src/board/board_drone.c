666f0f164e827e6015cd36d48ce615f9587b13c7
eric.park <pjsin865@coasia.com>
2021-07-27 15:15:12 +0900

    drone_ref: add settings for AXP228
    Signed-off-by: eric.park <pjsin865@coasia.com>
    Change-Id: I21c085d62f0adfb88d59d586e0b5c36cdbe4141d

--- src/board/board_drone.c
+++ src/board/board_drone.c
@@ -20,57 +20,86 @@
 #include <i2c_gpio.h>
 #include <axp228.h>
 
-#define AUTO_VOLTAGE_CONTROL			1
-#define ARM_VOLTAGE_CONTROL_SKIP		0
+/**********************************************
+ * PMIC I2C Info
+************************************************/
+#define PMIC_I2C_GPIO_GRP 3 // GPIOD
+#define PMIC_I2C_SCL 20 // SCL : GPIOD 20
+#define PMIC_I2C_SDA 16 // SDA : GPIOD 16
+#define PMIC_I2C_SCL_ALT 0 // SCL : ALT0
+#define PMIC_I2C_SDA_ALT 0 // SDA: ALT0
 
-#define AXP_I2C_GPIO_GRP 			3 				// GPIOD
-#define AXP_I2C_SCL 				20				// SCL : GPIOD 20
-#define AXP_I2C_SDA 				16				// SDA : GPIOD 16
-#define AXP_I2C_SCL_ALT				0				// SCL : ALT0
-#define AXP_I2C_SDA_ALT				0				// SDA: ALT0
-
-/************************************************
+/**********************************************
  * Drone Board (PMIC: AXP228)  - Reference 2016.04.05
- * ARM		: 1.25V (DC2)
- * CORE		: 1.1V (DC3)
- ************************************************/
+ * ARM      : 1.25V
+ * CORE     : 1.1V
+ * SYS      : 3.3V
+ * DDR_IO   : 1.5V
+ * DDR      : 1.5V
+************************************************/
+#define PMIC_DEF_ARM_VOL 1250000
+#define PMIC_DEF_CORE_VOL 1100000
+#define PMIC_DEF_3P3_VOL 3300000
+#define PMIC_DEF_DDRIO_VOL 1500000
+#define PMIC_DEF_DDR_VOL 1500000
+
 void pmic_board_init(void)
 {
 	char data[4];
 
-	I2C_INIT(AXP_I2C_GPIO_GRP, AXP_I2C_SCL, AXP_I2C_SDA,
-		AXP_I2C_SCL_ALT, AXP_I2C_SDA_ALT);
+	I2C_INIT(PMIC_I2C_GPIO_GRP, PMIC_I2C_SCL, PMIC_I2C_SDA,
+		PMIC_I2C_SCL_ALT, PMIC_I2C_SDA_ALT);
 
-	axp228_read(0x80, data, 1);
+	/* REG 80H : DC-DC Work mode
+	 *       bit 0 : DCDC1
+	 *           1 : DCDC2
+	 *           2 : DCDC3
+	 *           3 : DCDC4
+	 *           4 : DCDC5
+	 *   value 0 : PFM/PWM auto switch
+	 *           1 : fixed PWM */
+	axp228_read(AXP228_REG_DCDC_WORK_MODE, data, 1);
 	data[0] = (data[0] & 0x1F) | DCDC_SYS | DCDC_DDR;
-	axp228_write(0x80, data, 1);
+	axp228_write(AXP228_REG_DCDC_WORK_MODE, data, 1);
 
-	/* ARM voltage change */
-#if (ARM_VOLTAGE_CONTROL_SKIP == 0)
+	/* Set voltage of DCDC2(VCC_ARM) */
 	data[0] = axp228_get_dcdc_step(
-			AXP228_DEF_DDC2_VOL, AXP228_DEF_DDC234_VOL_STEP,
-			AXP228_DEF_DDC234_VOL_MIN, AXP228_DEF_DDC24_VOL_MAX);
+		PMIC_DEF_ARM_VOL, AXP228_DEF_DDC234_VOL_STEP,
+		AXP228_DEF_DDC234_VOL_MIN, AXP228_DEF_DDC24_VOL_MAX);
 	axp228_write(AXP228_REG_DC2VOL, data, 1);
-#endif
 
-	/* Core Voltage Change */
+	/* Set voltage of DCDC3(VCC_CORE) */
 	data[0] = axp228_get_dcdc_step(
-			AXP228_DEF_DDC3_VOL, AXP228_DEF_DDC234_VOL_STEP,
-			AXP228_DEF_DDC234_VOL_MIN, AXP228_DEF_DDC3_VOL_MAX);
+		PMIC_DEF_CORE_VOL, AXP228_DEF_DDC234_VOL_STEP,
+		AXP228_DEF_DDC234_VOL_MIN, AXP228_DEF_DDC3_VOL_MAX);
 	axp228_write(AXP228_REG_DC3VOL, data, 1);
+
+	/*
+	 * VCC3P3_CORE(OTP:3.3V), VCC1P5_DDRIO(OTP:1.5V), VCC1P5_DDR(OTP:1.5V)
+	 * It is not set as it is the same as the OTP setting.
+	 * If it is different from OTP, please set it.
+	 */
 #if 0
-	/* Set voltage of DCDC4. */
-	data[0] = axp228_get_dcdc_step(AXP228_DEF_DDC4_VOL,
-		AXP228_DEF_DDC234_VOL_STEP, AXP228_DEF_DDC234_VOL_MIN, AXP228_DEF_DDC24_VOL_MAX);
+	/* Set voltage of DCDC1(VCC3P3_CORE) */
+	data[0] = axp228_get_dcdc_step(
+		PMIC_DEF_3P3_VOL, AXP228_DEF_DDC1_VOL_STEP,
+		AXP228_DEF_DDC1_VOL_MIN, AXP228_DEF_DDC1_VOL_MAX);
+	axp228_write(AXP228_REG_DC1VOL, data, 1);
+
+	/* Set voltage of DCDC4(VCC1P5_DDRIO) */
+	data[0] = axp228_get_dcdc_step(
+		PMIC_DEF_DDRIO_VOL, AXP228_DEF_DDC234_VOL_STEP,
+		AXP228_DEF_DDC234_VOL_MIN, AXP228_DEF_DDC24_VOL_MAX);
 	axp228_write(AXP228_REG_DC4VOL, data, 1);
 
-	// Set voltage of DCDC5.
-	data[0] = axp228_get_dcdc_step(AXP228_DEF_DDC5_VOL,
-		AXP228_DEF_DDC5_VOL_STEP, AXP228_DEF_DDC5_VOL_MIN, AXP228_DEF_DDC5_VOL_MAX);
+	/* Set voltage of DCDC5(VCC1P5_DDR) */
+	data[0] = axp228_get_dcdc_step(
+		PMIC_DEF_DDR_VOL, AXP228_DEF_DDC5_VOL_STEP,
+		AXP228_DEF_DDC5_VOL_MIN, AXP228_DEF_DDC5_VOL_MAX);
 	axp228_write(AXP228_REG_DC5VOL, data, 1);
+
 #endif
 	I2C_DEINIT();
-
-	return;
 }
-#endif
+
+#endif /* PMIC_ON */
